b6e9cc0f1d0f9ccdec83fef97ed1d288c0790d8b
Joseph Mirabel <jmirabel@laas.fr>
2017-11-08 14:10:49 +0100

    Update smach_initializer and fox CMakeLists.txt

--- src/hpp_ros/smach_initializer/play_path.py
+++ src/hpp_ros/smach_initializer/play_path.py
@@ -1,15 +1,17 @@
-import smach
-import smach_ros
+import smach, smach_ros, rospy
 from std_msgs.msg import UInt32, Empty, String
 from hpp_ros_interface.srv import *
+from hpp_ros_interface.msg import *
+from hpp_ros_interface.client import HppClient
 import std_srvs.srv
-import ros_tools
+from hpp_ros_interface import ros_tools
 
-_outcomes = ("succeeded", "aborted", "preempted")
+_outcomes = ["succeeded", "aborted", "preempted"]
 
 class InitializePath(smach.State):
     hppTargetPubDict = {
             "read_path": [ UInt32, 1 ],
+            "read_subpath": [ ReadSubPath, 1 ],
             }
     hppTargetSrvDict = {
             "reset_topics": [ std_srvs.srv.Empty, ],
@@ -20,21 +22,42 @@ class InitializePath(smach.State):
             }
 
     def __init__(self):
-        super(InitializePath, self).__init__(self, outcomes=_outcomes)
+        super(InitializePath, self).__init__(
+                outcomes = _outcomes,
+                input_keys = [ "pathId", "times", "currentSection" ],
+                output_keys = [ "transitionId", "currentSection" ],
+                )
 
         self.targetSrv = ros_tools.createServices (self, "/hpp/target", self.hppTargetSrvDict, serve=False)
         self.targetPub = ros_tools.createTopics (self, "/hpp/target", self.hppTargetPubDict, subscribe=False)
+        self.hppclient = HppClient (False)
 
     def execute (self, userdata):
         # TODO makeSot
-        self.targetSrv["reset_topics"](std_srvs.srv.Empty())
+        # self.targetSrv["reset_topics"](std_srvs.srv.Empty())
         # # TODO Set the topics specifying the targets
         # self.targetSrv["add_center_of_mass"](SetStringRequest("name"))
         # self.targetSrv["add_center_of_mass_velocity"](SetStringRequest("name"))
         # # ...
 
-        self.targetPub["read_path"].publish (UInt32 (userdata.pathId))
-        return _outcomes[0]
+        userdata.currentSection += 1
+        if userdata.currentSection + 1 >= len(userdata.times):
+            # TODO Change SOT to id -1
+            return _outcomes[0]
+        start = userdata.times[userdata.currentSection]
+        length = userdata.times[userdata.currentSection + 1] - start
+
+        hpp = self.hppclient._hpp()
+        manip = self.hppclient._manip()
+        print userdata.pathId, start + length / 2
+        # rospy.sleep(3)
+        userdata.transitionId = manip.problem.edgeAtParam(userdata.pathId, start + length / 2)
+        # rospy.sleep(3)
+        # userdata.transitionId = manip.problem.edgeAtParam(userdata.pathId, start)
+
+        self.targetPub["read_subpath"].publish (ReadSubPath (userdata.pathId, start, length))
+        rospy.sleep(3)
+        return _outcomes[2]
 
 class PlayPath (smach.State):
     hppTargetPubDict = {
@@ -44,20 +67,30 @@ class PlayPath (smach.State):
             "sot_hpp": {
                 "error": [ String, "handleError" ],
                 "interrupt": [ String, "handleInterrupt" ],
-                }
+                },
             "hpp" : {
                 "target": {
                     "publish_done": [ Empty, "handleFinished" ]
                     }
                 }
             }
+    serviceProxiesDict = {
+            'sot': {
+                'plug_sot': [ PlugSot, ]
+                }
+            }
 
     def __init__(self):
-        super(PlayPath, self).__init__(self, outcomes=["succeeded"])
+        super(PlayPath, self).__init__(
+                outcomes = _outcomes,
+                input_keys = [ "transitionId", ],
+                output_keys = [ ])
 
         self.targetPub = ros_tools.createTopics (self, "/hpp/target", self.hppTargetPubDict, subscribe=False)
-        self.subscribers = self._createTopics ("/sot_hpp", self.subscribersDict, subscribe=True)
+        self.subscribers = ros_tools.createTopics (self, "", self.subscribersDict, subscribe=True)
+        self.serviceProxies = ros_tools.createServices (self, "", PlayPath.serviceProxiesDict, serve=False)
 
+        self.done = False
         self.error = None
         self.interruption = None
 
@@ -74,62 +107,114 @@ class PlayPath (smach.State):
 
     def execute(self, userdata):
         # TODO Check that there the current SOT and the future SOT are compatible ?
-        # TODO switch from old SOT to new SOT
+        status = self.serviceProxies['sot']['plug_sot'](userdata.transitionId)
+        if not status.success:
+            rospy.logerr(status.msg)
+            return _outcomes[1]
+
         self.done = False
-        self.targetPub["publish"](Empty())
+        self.targetPub["publish"].publish(Empty())
         # Wait for errors or publish done
         rate = rospy.Rate (1000)
-        userdata.error = None
-        userdata.interruption = None
         while not self.done:
             if self.error is not None:
                 # TODO handle error
-                rospy.logerror(str(self.error))
-                userdata.error = self.error
+                rospy.logerr(str(self.error))
+                self.error = None
                 return _outcomes[1]
             rate.sleep()
         if self.interruption is not None:
-            userdata.interruption = self.interruption
+            rospy.logerr(str(self.interruption))
+            self.interruption = None
             return _outcomes[2]
         return _outcomes[0]
 
-class ErrorHandling (smach.State):
-    subscribersDict = {
-            "sot_hpp": {
-                "error": [ String, "handleError" ],
-                }
-            "hpp" : {
-                "target": {
-                    "publish_done": [ Empty, "handleFinished" ]
+class WaitForInput(smach.State):
+    serviceProxiesDict = {
+            'sot': {
+                'request_hpp_topics': [ std_srvs.srv.Trigger, ]
+                },
+            'hpp': {
+                'target': {
+                    "reset_topics": [ std_srvs.srv.Empty, ],
                     }
                 }
             }
+    subscribersDict = {
+            "start_path": [ UInt32, "start" ],
+            }
 
     def __init__(self):
-        super(PlayPath, self).__init__(self, outcomes=["succeeded"])
+        super(WaitForInput, self).__init__(
+                outcomes = [ "succeeded", "aborted" ],
+                input_keys = [ ],
+                output_keys = [ "pathId", "times", "currentSection" ],
+                )
 
-        self.targetPub = ros_tools.createTopics (self, "/hpp/target", self.hppTargetPubDict, subscribe=False)
-        self.subscribers = self._createTopics ("/sot_hpp", self.subscribersDict, subscribe=True)
+        self.subscribers = ros_tools.createTopics (self, "/sm_sot_hpp", self.subscribersDict, subscribe = True)
+        self.services = ros_tools.createServices (self, "", self.serviceProxiesDict, serve = False)
+        self.hppclient = HppClient (False)
+        self.request = None
 
-        self.error = None
+    def start (self, msg):
+        self.request = msg
+        rospy.loginfo("Requested to start path " + str(self.request.data))
 
-    def handleError (self, msg):
-        self.error = msg
-
-    def handleFinished (self, msg):
-        self.done = True
-
-    def execute(self, userdata):
-        # TODO Check that there the current SOT and the future SOT are compatible ?
-        # TODO switch from old SOT to new SOT
-        self.done = False
-        self.targetPub["publish"](Empty())
-        # Wait for errors or publish done
+    def execute (self, userdata):
         rate = rospy.Rate (1000)
-        while not self.done:
-            if self.error is not None:
-                # TODO handle error
-                rospy.logerror(str(self.error))
-                return _outcomes[1]
+        while self.request is None:
             rate.sleep()
-        return _outcomes[0]
+        pid = self.request.data
+        self.request = None
+        userdata.pathId = pid
+        try:
+            hpp = self.hppclient._hpp()
+            qs, ts = hpp.problem.getWaypoints(pid)
+            userdata.times = ts
+            userdata.currentSection = -1
+            self.services['hpp']['target']['reset_topics']()
+            self.services['sot']['request_hpp_topics']()
+            # TODO check that qs[0] and the current robot configuration are
+            # close
+        except Exception, e:
+            rospy.logerr("Failed " + str(e))
+            return "aborted"
+        return "succeeded"
+
+def makeStateMachine():
+    sm = smach.StateMachine (outcomes = _outcomes)
+
+    with sm:
+        smach.StateMachine.add ('WaitForInput', WaitForInput(),
+                transitions = {
+                    "succeeded": 'Init',
+                    "aborted": "WaitForInput" },
+                remapping = {
+                    "pathId": "pathId",
+                    "times": "times",
+                    "currentSection": "currentSection",
+                    })
+        smach.StateMachine.add ('Init', InitializePath(),
+                transitions = {
+                    "succeeded": "WaitForInput",
+                    "aborted": "aborted",
+                    "preempted": 'Play'},
+                remapping = {
+                    "pathId": "pathId",
+                    "times": "times",
+                    "transitionId": "transitionId",
+                    "currentSection": "currentSection",
+                    })
+        smach.StateMachine.add ('Play', PlayPath(),
+                transitions = {
+                    "succeeded": 'Init',
+                    "aborted": "WaitForInput",
+                    "preempted": "WaitForInput"},
+                remapping = {
+                    "transitionId": "transitionId",
+                    })
+
+    sm.set_initial_state(["WaitForInput"])
+
+    sis = smach_ros.IntrospectionServer('sm_sot_hpp', sm, '/SM_SOT_HPP')
+    return sm, sis
--- src/hpp_ros/trajectory_publisher.py
+++ src/hpp_ros/trajectory_publisher.py
@@ -4,6 +4,7 @@ import numpy as np
 from hpp_ros_interface.client import HppClient
 from hpp_ros_interface.msg import *
 from hpp_ros_interface.srv import *
+import ros_tools
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from sensor_msgs.msg import JointState
 import Queue
@@ -108,6 +109,7 @@ class HppOutputQueue(HppClient):
             "hpp": {
                 "target": {
                     "read_path": [ UInt32, "read" ],
+                    "read_subpath": [ ReadSubPath, "readSub" ],
                     "publish": [ Empty, "publish" ]
                     },
                 },
@@ -136,9 +138,13 @@ class HppOutputQueue(HppClient):
             self.MsgType = MsgType
             self.data = data
 
-        def read (self, hpp, pathId, time):
+        def init (self, hpp):
+            msg = self.read (hpp)
+            self.pub.publish(msg)
+
+        def read (self, hpp):
             # return self.MsgType (self.reader(hpp, pathId, time, self.data))
-            return self.reader(hpp, pathId, time, self.data)
+            return self.reader(hpp, self.data)
 
         def publish (self, msg):
             self.pub.publish(msg)
@@ -146,9 +152,8 @@ class HppOutputQueue(HppClient):
         def __init__ (self, parent):
             self.parent = parent
 
-        def read (self, hpp, pathId, time, uv):
+        def read (self, hpp, uv):
             if uv:
-                hpp.robot.setCurrentConfig ( hpp.problem.configAtParam(pathId, time) )
                 pos = list()
                 for j, prefix, o in self.parent.viewer.robotBodies:
                     pos.append ( (prefix + o, hpp.robot.getLinkPosition (o) ) )
@@ -165,15 +170,17 @@ class HppOutputQueue(HppClient):
     def __init__ (self):
         super(HppOutputQueue, self).__init__ (withViewer = True)
 
-        self.frequency = 100 # Hz
+        self.frequency = 75 # Hz
         self.viewerFreq = 25 # Hz
-        self.queue = Queue.Queue (100)
+        self.queue_size = 10 * self.frequency
+        self.queue = Queue.Queue (self.queue_size)
 
         self.setJointNames (SetJointNamesRequest(self._hpp().robot.getJointNames()))
 
         self.subscribers = self._createTopics ("", self.subscribersDict, True)
         self.services = self._createServices ("", self.servicesDict, True)
         self.pubs = ros_tools.createTopics(self, "/hpp/target", self.publishersDist, subscribe = False)
+        self.reading = False
 
         self.resetTopics ()
 
@@ -183,6 +190,10 @@ class HppOutputQueue(HppClient):
                 self.Topic (self._readConfigAtParam, "position", Vector),
                 self.Topic (self._readConfigAtParam, "velocity", Vector),
                 ]
+        hpp = self._hpp()
+        self.topics[1].init(hpp)
+        self.topics[2].init(hpp)
+        rospy.loginfo("Reset topics")
         if msg is not None:
             return std_srvs.srv.EmptyResponse()
 
@@ -195,6 +206,8 @@ class HppOutputQueue(HppClient):
         self.topics.append (
                 self.Topic (self._readCenterOfMass, n, Vector3, data = comName),
                 )
+        self.topics[-1].init(self._hpp())
+        rospy.loginfo("Add topic " + n)
         return SetStringResponse(True)
 
     def addCenterOfMassVelocity (self, req):
@@ -206,18 +219,24 @@ class HppOutputQueue(HppClient):
         self.topics.append (
                 self.Topic (self._readCenterOfMassVelocity, n, Vector3, data = comName),
                 )
+        self.topics[-1].init(self._hpp())
+        rospy.loginfo("Add topic " + n)
         return SetStringResponse(True)
 
     def addOperationalFrame (self, req):
         # TODO check that frame exists
         n = "op_frame/" + req.value
         self.topics.append (self.Topic (self._readJointPosition, n, Transform, data = req.value))
+        self.topics[-1].init(self._hpp())
+        rospy.loginfo("Add topic " + n)
         return SetStringResponse(True)
 
     def addOperationalFrameVelocity (self, req):
         # TODO check that frame exists
         n = "velocity/op_frame/" + req.value
         self.topics.append (self.Topic (self._readJointVelocity, n, Vector, data = req.value))
+        self.topics[-1].init(self._hpp())
+        rospy.loginfo("Add topic " + n)
         return SetStringResponse(True)
 
     def setJointNames (self, req):
@@ -247,11 +266,11 @@ class HppOutputQueue(HppClient):
             # print self.joint_selection
         except:
             return SetJointNamesResponse(False)
+        rospy.loginfo("Joint names set to " + str(self.jointNames))
         return SetJointNamesResponse(True)
 
-    def _readConfigAtParam (self, client, pathId, time, data):
-        qin = client.problem.configAtParam (pathId, time)
-        client.robot.setCurrentConfig(qin)
+    def _readConfigAtParam (self, client, data):
+        qin = client.robot.getCurrentConfig()
         qout = list()
         # vout = list()
         for segment in self.joint_selection[0]:
@@ -260,41 +279,42 @@ class HppOutputQueue(HppClient):
             # vout.extend(vin[segment[0]:segment[1]])
         return Vector(qout)
 
-    def _readVelocityAtParam (self, client, pathId, time, data):
-        vin = client.problem.velocityAtParam (pathId, time)
-        client.robot.setCurrentVelocity(vin)
+    def _readVelocityAtParam (self, client, data):
+        vin = client.robot.getCurrentVelocity()
         vout = list()
         for segment in self.joint_selection[1]:
             vout.extend(vin[segment[0]:segment[1]])
         return Vector(vout)
 
-    def _readCenterOfMass (self, client, pathId, time, data):
+    def _readCenterOfMass (self, client, data):
         if data == "":
             v = client.robot.getCenterOfMass()
         else:
             v = client.robot.getPartialCom(data)
         return listToVector3(v)
 
-    def _readCenterOfMassVelocity (self, client, pathId, time, data):
+    def _readCenterOfMassVelocity (self, client, data):
         if data == "":
             v = client.robot.getCenterOfMassVelocity()
         else:
             v = client.robot.getVelocityPartialCom(data)
         return listToVector3(v)
 
-    def _readJointPosition (self, client, pathId, time, data):
+    def _readJointPosition (self, client, data):
         t = client.robot.getJointPosition(data)
         return listToTransform(t)
 
-    def _readJointVelocity (self, client, pathId, time, data):
+    def _readJointVelocity (self, client, data):
         t = client.robot.getJointVelocity(data)
         return Vector(t)
 
     def readAt (self, pathId, time, uv = False):
         hpp = self._hpp()
-        msgs = [ self.topics[0].read (hpp, pathId, time, uv), ]
+        hpp.robot.setCurrentConfig( hpp.problem.configAtParam (pathId, time))
+        hpp.robot.setCurrentVelocity( hpp.problem.velocityAtParam (pathId, time))
+        msgs = [ self.topics[0].read (hpp, uv), ]
         for topic in self.topics[1:]:
-            msgs.append (topic.read(hpp, pathId, time))
+            msgs.append (topic.read(hpp))
         self.queue.put (msgs, True)
 
     def publishNext (self):
@@ -303,15 +323,15 @@ class HppOutputQueue(HppClient):
             topic.publish (msg)
         self.queue.task_done()
 
-    def read (self, msg):
-        pathId = msg.data
+    def _read (self, pathId, start, L):
         from math import ceil, floor
-        hpp = self._hpp()
-        L = hpp.problem.pathLength(pathId)
-        N = int(ceil(L * self.frequency))
+        N = int(ceil(abs(L) * self.frequency))
         rospy.loginfo("Start reading path {} into {} points".format(pathId, N+1))
-        times = np.array(range(N+1), dtype=float) / self.frequency
+        self.reading = True
+        self.queue = Queue.Queue (self.queue_size)
+        times = (-1 if L < 0 else 1 ) *np.array(range(N+1), dtype=float) / self.frequency
         times[-1] = L
+        times += start
         Nv = int(ceil(float(self.frequency) / float(self.viewerFreq)))
         updateViewer = [ False ] * (N+1)
         if hasattr(self, "viewer"):
@@ -319,14 +339,26 @@ class HppOutputQueue(HppClient):
             updateViewer[-1] = True
         for t, uv in zip(times, updateViewer):
             self.readAt(pathId, t, uv)
-        self.pubs["read_path_done"].publish(msg)
+        self.pubs["read_path_done"].publish(UInt32(pathId))
         rospy.loginfo("Finish reading path {}".format(pathId))
+        self.reading = False
+
+    def read (self, msg):
+        pathId = msg.data
+        hpp = self._hpp()
+        L = hpp.problem.pathLength(pathId)
+        self._read (pathId, 0, L)
+
+    def readSub (self, msg):
+        self._read (msg.id, msg.start, msg.length)
 
     def publish(self, empty):
-        rospy.loginfo("Start publishing queue")
+        rospy.loginfo("Start publishing queue (size is {})".format(self.queue.qsize()))
         rate = rospy.Rate (self.frequency)
         while not self.queue.empty():
             self.publishNext()
             rate.sleep()
+        if self.reading:
+            rospy.logwarn("Stop publishing while still reading. Consider increasing the queue.")
         self.pubs["publish_done"].publish(Empty())
         rospy.loginfo("Finish publishing queue")
