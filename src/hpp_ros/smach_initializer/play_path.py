e8023dfa3c83a85a620ddba45a224e86c5278c87
Joseph Mirabel <jmirabel@laas.fr>
2017-11-13 18:00:16 +0100

    Update PlayPath

--- src/hpp_ros/smach_initializer/play_path.py
+++ src/hpp_ros/smach_initializer/play_path.py
@@ -1,5 +1,5 @@
 import smach, smach_ros, rospy
-from std_msgs.msg import UInt32, Empty, String
+from std_msgs.msg import UInt32, Empty, String, Float64
 from hpp_ros_interface.srv import *
 from hpp_ros_interface.msg import *
 from hpp_ros_interface.client import HppClient
@@ -33,16 +33,8 @@ class InitializePath(smach.State):
         self.hppclient = HppClient (False)
 
     def execute (self, userdata):
-        # TODO makeSot
-        # self.targetSrv["reset_topics"](std_srvs.srv.Empty())
-        # # TODO Set the topics specifying the targets
-        # self.targetSrv["add_center_of_mass"](SetStringRequest("name"))
-        # self.targetSrv["add_center_of_mass_velocity"](SetStringRequest("name"))
-        # # ...
-
         userdata.currentSection += 1
         if userdata.currentSection + 1 >= len(userdata.times):
-            # TODO Change SOT to id -1
             return _outcomes[0]
         start = userdata.times[userdata.currentSection]
         length = userdata.times[userdata.currentSection + 1] - start
@@ -54,7 +46,8 @@ class InitializePath(smach.State):
         # userdata.transitionId = manip.problem.edgeAtParam(userdata.pathId, start)
 
         self.targetPub["read_subpath"].publish (ReadSubPath (userdata.pathId, start, length))
-        rospy.sleep(3)
+        print "Start reading subpath. Waiting for one second."
+        rospy.sleep(1)
         return _outcomes[2]
 
 class PlayPath (smach.State):
@@ -65,6 +58,7 @@ class PlayPath (smach.State):
             "sot_hpp": {
                 "error": [ String, "handleError" ],
                 "interrupt": [ String, "handleInterrupt" ],
+                "control_norm_changed": [ Float64, "handleControlNormChanged" ],
                 },
             "hpp" : {
                 "target": {
@@ -91,6 +85,7 @@ class PlayPath (smach.State):
         self.done = False
         self.error = None
         self.interruption = None
+        self.control_norm_ok = False
 
     def handleError (self, msg):
         self.error = msg.data
@@ -103,6 +98,9 @@ class PlayPath (smach.State):
     def handleFinished (self, msg):
         self.done = True
 
+    def handleControlNormChanged (self, msg):
+        self.control_norm_ok = msg.data < 1e-2
+
     def execute(self, userdata):
         # TODO Check that there the current SOT and the future SOT are compatible ?
         status = self.serviceProxies['sot']['plug_sot'](userdata.transitionId)
@@ -111,6 +109,7 @@ class PlayPath (smach.State):
             return _outcomes[1]
 
         self.done = False
+        # self.control_norm_ok = False
         self.targetPub["publish"].publish(Empty())
         # Wait for errors or publish done
         rate = rospy.Rate (1000)
@@ -125,12 +124,16 @@ class PlayPath (smach.State):
             rospy.logerr(str(self.interruption))
             self.interruption = None
             return _outcomes[2]
+        print "Wait for event on /sot_hpp/control_norm_changed"
+        while not self.control_norm_ok:
+            rate.sleep()
         return _outcomes[0]
 
 class WaitForInput(smach.State):
     serviceProxiesDict = {
             'sot': {
-                'request_hpp_topics': [ std_srvs.srv.Trigger, ]
+                'request_hpp_topics': [ std_srvs.srv.Trigger, ],
+                'plug_sot': [ PlugSot, ]
                 },
             'hpp': {
                 'target': {
@@ -150,6 +153,7 @@ class WaitForInput(smach.State):
         self.hppclient = HppClient (False)
 
     def execute (self, userdata):
+        status = self.services['sot']['plug_sot'](-1)
         res = rospy.wait_for_message ("/sm_sot_hpp/start_path", UInt32)
         pid = res.data
         rospy.loginfo("Requested to start path " + str(pid))
@@ -161,8 +165,8 @@ class WaitForInput(smach.State):
             userdata.times = ts[0:1] + ts
             userdata.currentSection = -1
             # TODO this should not be necessary
-            # self.services['hpp']['target']['reset_topics']()
-            # self.services['sot']['request_hpp_topics']()
+            self.services['hpp']['target']['reset_topics']()
+            self.services['sot']['request_hpp_topics']()
             # TODO check that qs[0] and the current robot configuration are
             # close
         except Exception, e:
